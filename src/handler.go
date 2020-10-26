a99410ad4136a38c4de5e4a11927db2df58fbccc
qspors <qspors@gmail.com>
2020-10-26 18:41:57 -0400

    update

--- src/handler.go
+++ src/handler.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os/exec"
+)
 
+func main() {
+	getDriveInfo()
+}
+
+func getDriveInfo() {
+	out, err := exec.Command("lsblk -J -a").Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(out)
 }
