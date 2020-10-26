f8ddb9f18cad4ece4c9c1692b132bfef61897815
qspors <qspors@gmail.com>
2020-10-26 19:10:55 -0400

    update

--- src/handler.go
+++ src/handler.go
@@ -8,9 +8,15 @@ import (
 )
 
 type Drives struct {
-	driveName string `json:"name"`
-	driveSize string `json:"size"`
-	driveType string `json:"type"`
+	driveName  string `json:"name"`
+	majMin     string `json:"maj:min"`
+	rM         string `json:"rm"`
+	driveSize  string `json:"size"`
+	rO         string `json:"ro"`
+	driveType  string `json:"type"`
+	tyPe       string `json:"type"`
+	mountPoint string `json:"mountpoint"`
+	children   string `json:"children"`
 }
 
 func main() {
