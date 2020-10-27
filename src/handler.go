a48a0e4f2dce31961e7715ed016b7d5197eab003
qspors <qspors@gmail.com>
2020-10-26 23:02:28 -0400

    update

--- src/handler.go
+++ src/handler.go
@@ -7,21 +7,47 @@ import (
 	"os/exec"
 )
 
-func main() {
-	getDriveInfo()
+func UnmarshalWelcome(data []byte) (Welcome, error) {
+	var r Welcome
+	err := json.Unmarshal(data, &r)
+	return r, err
 }
 
-func getDriveInfo() {
+func (r *Welcome) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type Welcome struct {
+	Blockdevices []Blockdevice `json:"blockdevices"`
+}
+
+type Blockdevice struct {
+	Name       string        `json:"name"`
+	MajMin     string        `json:"maj:min"`
+	Rm         string        `json:"rm"`
+	Size       *string       `json:"size"`
+	Ro         string        `json:"ro"`
+	Type       Type          `json:"type"`
+	Mountpoint *string       `json:"mountpoint"`
+	Children   []Blockdevice `json:"children,omitempty"`
+}
+
+type Type string
+
+const (
+	Disk Type = "disk"
+	Loop Type = "loop"
+	Part Type = "part"
+)
+
+func main() {
 	out, err := exec.Command("lsblk", "-J", "-a").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	bytes, err := json.Marshal(out)
+	r, err := UnmarshalWelcome(out)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-
-	fmt.Println(string(bytes))
-
+	fmt.Println(r)
 }
