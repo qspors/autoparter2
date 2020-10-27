9fb4a5980bd2edeca1d0d6abb82abed58bfaf342
qspors <qspors@gmail.com>
2020-10-26 21:13:21 -0400

    update

--- src/handler.go
+++ src/handler.go
@@ -12,7 +12,7 @@ func main() {
 }
 
 func getDriveInfo() {
-	out, err := exec.Command("lsblk", "-P").Output()
+	out, err := exec.Command("lsblk", "-J", "--tree").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
