b9a846c769321c4c49534b1e531b1b84e5b730d9
qspors <qspors@gmail.com>
2020-10-26 21:01:58 -0400

    update

--- src/handler.go
+++ src/handler.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"os/exec"
@@ -12,28 +11,10 @@ func main() {
 }
 
 func getDriveInfo() {
-	type Devices struct {
-		Blockdevices string `json:"blockdevices"`
-	}
-	out, err := exec.Command("lsblk", "-J", "-a").Output()
+	out, err := exec.Command("lsblk", "-P").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%s", out)
-	for _, vals := range out {
-		str := fmt.Sprint(vals)
-		println(str)
-	}
-	bytes, err := json.Marshal(out)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var d Devices
-	err = json.Unmarshal(bytes, &d)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("%+v\n", d)
 
 }
