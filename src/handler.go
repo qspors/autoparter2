bb734ee44f818c57a17ae9bf283f90d067ad1d52
qspors <qspors@gmail.com>
2020-10-26 22:56:31 -0400

    update

--- src/handler.go
+++ src/handler.go
@@ -18,35 +18,12 @@ func getDriveInfo() {
 	}
 	str := string(out)
 	fmt.Println(str)
-	//
-	type Type string
-	const (
-		Disk Type = "disk"
-		Loop Type = "loop"
-		Part Type = "part"
-	)
-	type Blockdevice struct {
-		Name       string        `json:"name"`
-		MajMin     string        `json:"maj:min"`
-		Rm         string        `json:"rm"`
-		Size       *string       `json:"size"`
-		Ro         string        `json:"ro"`
-		Type       Type          `json:"type"`
-		Mountpoint *string       `json:"mountpoint"`
-		Children   []Blockdevice `json:"children,omitempty"`
-	}
-	type Welcome struct {
-		Blockdevices []Blockdevice `json:"blockdevices"`
-	}
 
-	//
 	bytes, err := json.Marshal(str)
 	if err != nil {
 		fmt.Println(err)
 	}
-	var d Welcome
 
-	err = json.Unmarshal(bytes, &d)
+	fmt.Println(bytes)
 
-	fmt.Printf("%+v\n", d)
 }
