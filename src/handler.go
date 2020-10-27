5f80ab36c0acfa9f24dc308ef8e353290291780e
qspors <qspors@gmail.com>
2020-10-26 20:41:27 -0400

    update

--- src/handler.go
+++ src/handler.go
@@ -1,22 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os/exec"
 )
 
 type Drives struct {
-	blockdevices []string `json:"blockdevices"`
-	//driveName  string `json:"name"`
-	//majMin     string `json:"maj:min"`
-	//rM         string `json:"rm"`
-	//driveSize  string `json:"size"`
-	//rO         string `json:"ro"`
-	//driveType  string `json:"type"`
-	//tyPe       string `json:"type"`
-	//mountPoint string `json:"mountpoint"`
-	//children   string `json:"children"`
+	Blockdevices []map[string]string `json:"blockdevices"`
 }
 
 func main() {
@@ -28,6 +20,15 @@ func getDriveInfo() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", out)
+	bytes, err := json.Marshal(out)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var d Drives
+	err = json.Unmarshal(bytes, &d)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", d)
 
 }
