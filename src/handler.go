620d209c21770aa5d644d15cd4ef0c19b77b74da
qspors <qspors@gmail.com>
2020-10-26 20:57:38 -0400

    update

--- src/handler.go
+++ src/handler.go
@@ -21,8 +21,7 @@ func getDriveInfo() {
 	}
 	fmt.Printf("%s", out)
 	for idx, vals := range out {
-		fmt.Printf("ix: %d, val: %s\n",
-			idx, vals)
+		fmt.Println(idx, vals)
 	}
 	bytes, err := json.Marshal(out)
 	if err != nil {
