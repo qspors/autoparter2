59e280d307b6e503b0fb393f8413f0f1c03e062f
y.yurov <qspors@gmail.com>
2020-10-30 15:29:38 -0400

    update

--- src/handler.go
+++ src/handler.go
@@ -201,8 +201,9 @@ func compareVolumeAndDrives(drives map[string]int64, volumes map[string]int64, f
 	for driveLabel, driveSize := range drives {
 		for dirName, dirSize := range volumes {
 			if driveSize == dirSize {
-				log.Printf("Action for drive: %s, dir: %s\n", driveLabel, dirName)
+
 				log.Println("####################################################")
+				log.Printf("Action for drive: %s, dir: %s\n", driveLabel, dirName)
 				doMountingActions(driveLabel, dirName, filesystem)
 				delete(volumes, dirName)
 				log.Println("####################################################")
