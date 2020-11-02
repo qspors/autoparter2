15b97749449a3f27961134e1c5fcdb87dbc58853
qspors <qspors@gmail.com>
2020-11-02 13:40:30 -0500

    update

--- src/handler.go
+++ src/handler.go
@@ -185,8 +185,8 @@ func compareVolumeAndDrives(drives map[string]int64, volumes map[string]int64, f
 				//volumeProcessing(driveLabel, dirName, filesystem)
 				delete(volumes, dirName)
 				log.Println("Processing completed")
+				break
 			}
-			continue
 		}
 
 	}
