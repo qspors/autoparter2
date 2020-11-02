189b89d8f66f19ea5192fd321fa411644fbabb0c
qspors <qspors@gmail.com>
2020-11-02 13:36:49 -0500

    update

--- src/handler.go
+++ src/handler.go
@@ -178,16 +178,17 @@ func serviceStatus(command string, services []string) {
 func compareVolumeAndDrives(drives map[string]int64, volumes map[string]int64, filesystem string) {
 	log.Println("#################### ! ! ! >  H E L L O  < ! ! ! ####################")
 	for driveLabel, driveSize := range drives {
+		log.Printf("Action for drive: %s\n", driveLabel)
 		for dirName, dirSize := range volumes {
 			if driveSize == dirSize {
 				log.Printf("Processing drive: %s, dir: %s , drivesize: %d, filesystem: %s\n", driveLabel, dirName, driveSize, filesystem)
 				volumeProcessing(driveLabel, dirName, filesystem)
 				delete(volumes, dirName)
 				log.Println("Processing completed")
-				continue
 			}
 
 		}
+
 	}
 }
 func volumeProcessing(label string, dir string, filesystem string) {
