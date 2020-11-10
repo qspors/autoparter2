630be97ff6ae9d5d602c76dbc5bc462a36a50e69
qspors <qspors@gmail.com>
2020-11-10 16:24:07 -0500

    vg

--- src/handler.go
+++ src/handler.go
@@ -199,7 +199,7 @@ func pvGroupCreate(label string) {
 func lvcCreate(mPoint string, size int64) {
 	points := strings.Split(mPoint, "/")
 	point := fmt.Sprintf("mountpoint_%s", points[len(points)-1])
-	newSize := fmt.Sprintf("%dG", strconv.FormatInt(size, 10))
+	newSize := fmt.Sprintf("%sG", strconv.FormatInt(size, 10))
 	fmt.Println(point)
 	fmt.Println(newSize)
 	_, err := exec.Command("lvcreate", "-n", point, "-L", newSize, "group1").Output()
